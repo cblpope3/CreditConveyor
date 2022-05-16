7bb73e7887abee7bb9c3a3ce56aa8cc4e6202b94
Nickolay Leonov <nickolayshmakolay@gmail.com>
2022-05-16 16:20:46 +0300

    Method 'calculatePSK' reworked. Now using correct formula, and calculation result is more accurate than in previous simplified formula. But still, result not 100% match with online-calculators result.

--- Conveyor/src/main/java/ru/leonov/conveyor/service/CreditCalculationService.java
+++ Conveyor/src/main/java/ru/leonov/conveyor/service/CreditCalculationService.java
@@ -9,6 +9,7 @@ import java.math.BigDecimal;
 import java.math.MathContext;
 import java.math.RoundingMode;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -106,7 +107,7 @@ public class CreditCalculationService {
         // график ежемесячных платежей (List<PaymentScheduleElement>)
         List<ModelsPaymentScheduleElementDTO> paymentSchedule = this.calculateMonthlyPaymentsSchedule(
                 creditAmount, creditTerm, creditRate.divide(BigDecimal.valueOf(1200), MathContext.DECIMAL64), monthlyPayment);
-        BigDecimal psk = this.calculatePSK(paymentSchedule, creditAmount, creditTerm);
+        BigDecimal psk = this.calculatePSK(paymentSchedule, creditAmount);
 
         credit.setMonthlyPayment(monthlyPayment);
         credit.setPaymentSchedule(paymentSchedule);
@@ -116,33 +117,155 @@ public class CreditCalculationService {
     }
 
     /**
-     * Calculation of full credit price.
+     * Calculation of full credit price with simplified formula.
      *
-     * @param paymentSchedule {@link List} of calculated payments.
-     * @param creditAmount    full credit amount.
-     * @param creditTerm      credit term in months.
+     * @param creditAmount     full credit amount, roubles.
+     * @param totalPayments    total payments for credit (debt and interest), roubles.
+     * @param creditTermMonths credit term, months.
      * @return calculated full credit price.
+     * @deprecated because calculation with this formula gives incorrect results.
      */
-    private BigDecimal calculatePSK(List<ModelsPaymentScheduleElementDTO> paymentSchedule,
-                                    BigDecimal creditAmount, int creditTerm) {
-
-        // PSK = 12 * 100 * ((s/s0) - 1) / n
+    @SuppressWarnings("unused")
+    @Deprecated(since = "1.0", forRemoval = false)
+    private BigDecimal calculatePSKSimplified(BigDecimal creditAmount, BigDecimal totalPayments, int creditTermMonths) {
+        // PSK = 100 * ((s/s0) - 1) / n
         // where
         // s - total payments sum
         // s0 - credit amount
-        // n - number of months of credit term
+        // n - credit term in years
 
-        //fixme calculation result is not matching with online-calculator result
-        BigDecimal totalPaymentsSum = paymentSchedule.stream()
+        BigDecimal creditTermYears = BigDecimal.valueOf(creditTermMonths).divide(BigDecimal.valueOf(12), MathContext.DECIMAL64);
+
+        BigDecimal overpayCoefficient = totalPayments.divide(creditAmount, MathContext.DECIMAL64).subtract(BigDecimal.ONE);
+        BigDecimal partialResult = overpayCoefficient
+                .divide(creditTermYears, MathContext.DECIMAL64);
+
+        return partialResult.multiply(BigDecimal.valueOf(100)).setScale(2, RoundingMode.HALF_UP);
+    }
+
+    /**
+     * Calculation of full credit price. Calculated by
+     * <a href="https://unicom24.ru/media/open/2/8/7e/87ef428f1c52a440b97e516d7dbd06c2.jpg">this</a> formula.
+     *
+     * @param paymentSchedule {@link List} of calculated payments.
+     * @param creditAmount    full credit amount.
+     * @return calculated full credit price.
+     */
+    private BigDecimal calculatePSK(List<ModelsPaymentScheduleElementDTO> paymentSchedule,
+                                    BigDecimal creditAmount) {
+
+        //making dates of payment collection
+        //first element is day of receipt of the loan, assuming that this is one month before first payment date.
+        List<LocalDate> paymentDates = new ArrayList<>();
+        paymentDates.add(paymentSchedule.get(0).getDate().minusMonths(1));
+        paymentDates.addAll(paymentSchedule.stream()
+                .map(ModelsPaymentScheduleElementDTO::getDate)
+                .toList());
+
+        //making list of all payments
+        //first payment is negative credit amount (represents obtaining of the loan)
+        List<BigDecimal> payments = new ArrayList<>();
+        payments.add(creditAmount.multiply(BigDecimal.valueOf(-1)));
+        payments.addAll(paymentSchedule.stream()
                 .map(ModelsPaymentScheduleElementDTO::getTotalPayment)
+                .toList());
+
+        //calculating total payments amount (including getting of the loan itself)
+        int paymentsNumber = payments.size();
+
+        int basePeriod = 30;
+        long basePeriodNumberInYear = Math.round(365.0 / basePeriod);
+
+        //pre-calculation of days number from loan obtaining date to given payment date.
+        List<Long> daysFromLoanObtained = new ArrayList<>();
+        for (int k = 0; k < paymentsNumber; k++) {
+            daysFromLoanObtained.add(paymentDates.get(0).until(paymentDates.get(k), ChronoUnit.DAYS));
+        }
+
+        //pre-calculation of Ek and Qk parameters for each payment
+        List<BigDecimal> ek = new ArrayList<>();
+        List<Long> qk = new ArrayList<>();
+
+        for (int k = 0; k < paymentsNumber; k++) {
+            //e[k] = (daysFromLoanObtained[k] % basePeriod) / basePeriod
+            ek.add((BigDecimal.valueOf(daysFromLoanObtained.get(k))
+                    .remainder(BigDecimal.valueOf(basePeriod)))
+                    .divide(BigDecimal.valueOf(basePeriod), MathContext.DECIMAL64));
+
+            //q[k] = Math.floor(daysFromLoanObtained[k] / basePeriod)
+            qk.add(daysFromLoanObtained.get(k) / basePeriod);
+        }
+
+        //calculation of 'i' coefficient (base period percent rate, whatever it means).
+        class ICalculator {
+
+            /**
+             * Method that calculates value of i. Contains 'while' loop that perform approximation to 'i' value, that
+             * is producing value of sum as close to zero as possible with given step size.
+             * @param stepSize increment amount of 'i' on every iteration step.
+             * @param i initial value of 'i'.
+             * @return value of 'i' that produce closest to zero value of sum.
+             */
+            BigDecimal calculateNewIValue(BigDecimal stepSize, BigDecimal i) {
+                BigDecimal zeroSum = BigDecimal.ONE;
+                int iterationLimit = 10000;
+
+                while (zeroSum.compareTo(BigDecimal.ZERO) > 0) {
+                    --iterationLimit;
+                    zeroSum = BigDecimal.ZERO;
+                    i = i.add(stepSize);
+
+                    for (int k = 0; k < paymentsNumber; k++) {
+                        //zeroSum += (payments[k] / ((1 + ek[k] * i) * (1 + i) ^ qk[k]))
+                        BigDecimal ekComponent = ek.get(k).multiply(i).add(BigDecimal.ONE);
+                        BigDecimal qkComponent = i.add(BigDecimal.ONE).pow(qk.get(k).intValue(), MathContext.DECIMAL64);
+                        zeroSum = zeroSum.add(
+                                payments.get(k).divide(ekComponent.multiply(qkComponent), MathContext.DECIMAL64));
+                    }
+
+                    if (iterationLimit < 0) {
+                        throw new ArithmeticException("Can't calculate PSK: iteration limit reached!");
+                    }
+                }
+
+                return i;
+            }
+        }
+
+        BigDecimal i = BigDecimal.ZERO;
+        BigDecimal iPrev = BigDecimal.valueOf(1000);
+        BigDecimal stepSize = BigDecimal.valueOf(0.01);
+        BigDecimal resultTolerance = BigDecimal.valueOf(0.0001);
+
+        //performing calculations of 'i' value with a successive increase in accuracy.
+        //assuming that calculated value of 'i' is accurate enough when difference between new calculated 'i' and
+        //it's previous value is less than required result tolerance (defined by resultTolerance variable).
+        while (i.subtract(iPrev).abs().compareTo(resultTolerance) > 0) {
+            iPrev = i;
+            stepSize = stepSize.divide(BigDecimal.TEN, MathContext.DECIMAL64);
+            i = new ICalculator().calculateNewIValue(stepSize, i);
+        }
+
+        //finally, calculating requested PSK.
+        return i.multiply(BigDecimal.valueOf(basePeriodNumberInYear))
+                .multiply(BigDecimal.valueOf(100))
+                .setScale(2, RoundingMode.HALF_UP);
+    }
+
+    /**
+     * Calculation of total credit payments (debt and interest) based on given payments schedule.
+     *
+     * @param paymentSchedule credit payments schedule.
+     * @return sum of all credit payments.
+     */
+    private BigDecimal calculateTotalCreditPayments(List<ModelsPaymentScheduleElementDTO> paymentSchedule) {
+        //assuming that in payments schedule can be the loan itself
+        //therefore, counting only positive payments
+        return paymentSchedule.stream()
+                .map(ModelsPaymentScheduleElementDTO::getTotalPayment)
+                .filter(payment -> payment.compareTo(BigDecimal.ZERO) > 0)
                 .reduce(BigDecimal.valueOf(0),
                         BigDecimal::add);
-
-        BigDecimal overpayCoefficient = totalPaymentsSum.divide(creditAmount, MathContext.DECIMAL64);
-        BigDecimal partialResult = overpayCoefficient.subtract(BigDecimal.valueOf(1))
-                .divide(BigDecimal.valueOf(creditTerm), MathContext.DECIMAL64);
-
-        return partialResult.multiply(BigDecimal.valueOf(1200)).setScale(2, RoundingMode.HALF_UP);
     }
 
     /**
