45badd69847c29e5df881ed43e9e4733fd745c68
Nickolay Leonov <nickolayshmakolay@gmail.com>
2022-05-20 17:00:15 +0300

    Removed 'hello world!' notification.

--- Conveyor/src/main/java/ru/leonov/conveyor/CreditConveyorApplication.java
+++ Conveyor/src/main/java/ru/leonov/conveyor/CreditConveyorApplication.java
@@ -8,7 +8,5 @@ public class CreditConveyorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CreditConveyorApplication.class, args);
-        //todo remove this system.out
-        System.out.println("i'm working!");
     }
 }
