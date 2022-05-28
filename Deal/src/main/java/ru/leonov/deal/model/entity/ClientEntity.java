3c2ae77c7d4979e2b0996dd4187635d4f8fcaf60
Nickolay Leonov <nickolayshmakolay@gmail.com>
2022-05-28 15:43:48 +0300

    Added javadocs. Descriptions added for ClientEntity, EmploymentRecord and PassportRecord classes.

--- Deal/src/main/java/ru/leonov/deal/model/entity/ClientEntity.java
+++ Deal/src/main/java/ru/leonov/deal/model/entity/ClientEntity.java
@@ -11,59 +11,113 @@ import ru.leonov.deal.model.record.PassportRecord;
 import javax.persistence.*;
 import java.time.LocalDate;
 
+/**
+ * Entity that represents client as object that stored in table "clients" in database.
+ */
 @Entity
 @Getter
 @Setter
 @Table(name = "clients")
 @TypeDef(name = "jsonb", typeClass = JsonBinaryType.class)
 public class ClientEntity {
-    @Column(name = "last_name", nullable = false)
-    private String lastName;
 
+    /**
+     * First name of client.
+     */
     @Column(name = "first_name", nullable = false)
     private String firstName;
 
+    /**
+     * Last name of client.
+     */
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    /**
+     * Middle name of client. Nullable.
+     */
     @Column(name = "middle_name")
     private String middleName;
 
+    /**
+     * Client's birthdate.
+     */
     @Column(name = "birth_date", nullable = false)
     private LocalDate birthDate;
 
+    /**
+     * Client's e-mail address.
+     */
     @Column(name = "email", nullable = false)
     private String email;
 
+    /**
+     * Client's gender. Nullable.
+     *
+     * @see Gender
+     */
     @Enumerated(EnumType.STRING)
     @Column(name = "gender")
     private Gender gender;
 
+    /**
+     * Client's martial status. Nullable.
+     *
+     * @see MartialStatus
+     */
     @Enumerated(EnumType.STRING)
     @Column(name = "martial_status")
     private MartialStatus maritalStatus;
 
+    /**
+     * Number of dependent persons (children etc.). Nullable.
+     */
     @Column(name = "dependent_amount")
     private Integer dependentAmount;
 
+    /**
+     * Information about client's passport. Nullable.
+     *
+     * @see PassportRecord
+     */
     @Type(type = "jsonb")
     @Column(name = "passport", columnDefinition = "jsonb")
     private PassportRecord passport;
 
+    /**
+     * Information about client's job. Nullable.
+     *
+     * @see EmploymentRecord
+     */
     @Type(type = "jsonb")
     @Column(name = "employment", columnDefinition = "jsonb")
     private EmploymentRecord employment;
 
+    /**
+     * Client's bank account number. Nullable.
+     */
     @Column(name = "account")
     private Long account;
 
+    /**
+     * Client's id in database. Primary key, autoincrement.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "client_id_generator")
     @SequenceGenerator(name = "client_id_generator", sequenceName = "clients_sequence", allocationSize = 1)
     private Long id;
 
+    /**
+     * Client's gender enum. Possible values are: 'MALE', 'FEMALE' or 'NON_BINARY'.
+     */
     @SuppressWarnings("unused")
     public enum Gender {
         MALE, FEMALE, NON_BINARY
     }
 
+    /**
+     * Client's martial status enum. Possible values are: 'MARRIED', 'DIVORCED', 'SINGLE', 'WIDOW_WIDOWER'.
+     */
     @SuppressWarnings("unused")
     public enum MartialStatus {
         MARRIED, DIVORCED, SINGLE, WIDOW_WIDOWER
--- Deal/src/main/java/ru/leonov/deal/model/record/PassportRecord.java
+++ Deal/src/main/java/ru/leonov/deal/model/record/PassportRecord.java
@@ -6,12 +6,31 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+/**
+ * POJO that contain information about client's passport.
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class PassportRecord {
+
+    /**
+     * Client's passport series.
+     */
     private Integer series;
+
+    /**
+     * Client's passport number.
+     */
     private Integer number;
+
+    /**
+     * Client's passport issue date.
+     */
     private LocalDate issueDate;
+
+    /**
+     * Name of client's passport issue organization.
+     */
     private String issueBranch;
 }
